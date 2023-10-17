517d7a0dc994ff00dc3b62b8c8b080fe0d03f27c
https://github.com <bilalbaydur@gmail.com>
2023-10-17 22:05:45 +0300

    Edit CV

---
+++ Information.js
@@ -0,0 +1,310 @@
+
+//Türkçe CV içeriği
+const CvDataTR = {
+      img:"BilalBaydur_img.jpeg",
+      Name: "Bilal",
+      SurName: "Baydur",
+      Gender: "Erkek",
+      MilitaryService: "Yapıldı",
+      DriversLicence: "B",
+      Nationality: "Türkiye Cumhuriyeti",
+      Email: "xxxxxxx@gmail.com",
+      Phone: 5555555555,
+      Address: "İstanbul / Türkiye",
+      CoverLetter: "Yazılım hayatım boyunca farklı sorumluluk alanlarında görev almış biri olarak, "+
+        "mesleğe ilk başladığım günkü öğrenme isteğimi ve hevesimi canlı tutuyorum. Bu nedenle şirket " + 
+        "içi eğitime önem veren, huzurlu bir çalışma ortamı sunabilen, en önemlisi bunu şirket "+ 
+        "kültürü olarak benimsemiş bir takımın içinde yer alarak, büyük resmin bir parçası olmak istiyorum.<br/><br/>" +
+        "Yazılım geliştirme süreçlerinin proje başarısına etkisini, yüksek lisans dönem projesi olarak "+
+        "araştırırken, içinde yer aldığım birçok proje ve takımda gözlemleme fırsatı bulduğum, "+
+        "iletişim veya iletişim eksikliğinin yazılım geliştirme süreçlerine olumlu veya olumsuz "+
+        "etkilerinin, proje başarısına en az süreçlerin kendisi kadar etki ettiğini bir kez daha " + 
+        "anlama fırsatı buldum. Bu nedenle iletişime hak ettiği değeri verdiğimi ve bu yönümü güçlü " + 
+        "tutmaya çalıştığımı söyleyebilirim.",
+      Skills:[
+          "Delphi",
+          ".Net Core",
+          "Php",
+          "MsSql",
+          "MySql",
+          "Web Servis, Rest Api",
+          "Entity Framework",
+          "Azure Devops",
+          "Git, Tfs, Svn",
+          "Html, Css, Javascript",
+          "Xml, Xslt",
+          "Postman, Soap UI",
+          "Xamarin"
+      ],
+      Experiences: [
+          {
+            Date: "04.2014 -",
+            Pozision: "YAZILIM MÜDÜRÜ",
+            Company: "X YAZILIM A.Ş.",
+            JobDescription: "İş tanımımı, yazılım geliştirme ekibinin sorumlulugunu üstlenerek, " +
+                  "şirket projelerinin zamanında ve sorunsuz teslimini sağlamak ve yazılım " +
+                  "geliştirme yaşam döngüsü boyunca, yazılım geliştirme maliyetlerini düşürmek " +
+                  "ve bakım sürecini kolaylaştırmak için kod standartlarını ve kalitesini belirlenen "+ 
+                  "standartlarda olmasını sağlamak için yazılan kod kalitesini denetlemek ve tabiki projelere "+
+                  "kod yazarak katkı sağlamak, şeklinde nitelendirebilirim. <br/><br/>"+
+                  "Bu süreçte, Erp, Crm, Dijital Dönüşüm, Orduevi, Sinema gibi bir çok farklı sektör için web, "+
+                  "masaüstü ve mobil  projenin geliştirme süreçlerinde aktif olarak yer aldım." ,
+            Skills:  [
+                "Azure Devops",
+                "Git",
+                "Delphi",
+                "C#, .Net Core, Entity Framework, Wpf",
+                "MsSql",
+                "Oracle (Entegrasyon ihtiyaçları için)",
+                "Xamarin",
+                "Web Servis",
+                "Dijital Dönüşüm (e-fatura, e-irsaliye, e-ihracat, E-Defter)",
+                "Html, Css, Javascript"
+              ]
+          },
+          {
+            Date: "05.2009 - 04.2014",
+            Pozision: "YAZILIM UZMANI",
+            Company: "Y YAZILIM A.Ş.",
+            JobDescription: "Genel olarak delhpi ile geliştirilmiş projelerin, müşteri ihtiyaçları doğrultusunda geliştirilmesi ile sorumluydum. Delphi dışında c# ile yazılmış projelerin geliştirilmesi veya ihtiyaçlar doğrultusunda yeni projelerin web, masaüstü veya mobil uygulama olarak geliştirilmesinde rol aldım. Veritabanı olarak ağırlıklı olarak MsSql kullandım.",
+            Skills:[
+                "Delphi",
+                "C#, .Net, Entity Framework",
+                "MsSql",
+                "Oracle (Entegrasyon ihtiyaçları için)",
+                "TFS (Team Foundation Server)",
+                "Xamarin",
+                "Web Servis",
+                "Dijital Dönüşüm (e-fatura)",
+                "Html, Css, Javascript"
+              ]
+          },
+          {
+            Date: "02.2007 - 02.2009",
+            Pozision: "YAZILIM UZMANI",
+            Company: "Z YAZILIM",
+            JobDescription: "Genel olarak delphi ile yazılmış olan projelerin geliştirlmesinde görev aldım. "+
+                "Delphi ile yazılmış olan teknik servis programını php ile web ortamına taşıma sürecinde "+
+                "aktif rol aldım.Yazılımm geliştirme dışında, müşterilere geliştirdiğimiz programlarla ilgili "+  
+                "teknik destek verdim.<br/><br/>" +
+                "Bu süreçte, Kuru temizleme programı, Teknik destek programı, Kişisel asistan programı gibi çeşitli projelerde  web ve masa üstü programlar geliştirdim.",
+            Skills: [
+                "Delphi",
+                "MsSql, MySql, Access",
+                "Svn (Kod Takip)",
+                "Php",
+                "Html, Css, Javascript"
+              ]
+          }
+        ],
+      Educations: [
+          {
+            Date: "09.2021 - 02.2023",
+            Departman: "YÖNETİM BİLİŞİM SİSTEMLERİ",
+            School: "AHMET YESEVİ UNİVERSİTESİ"
+          },
+          {
+            Date: "09.2007 - 06.2010",
+            Departman: "İŞLETME",
+            School: "ANADOLU UNİVERSİTESİ"
+          }
+        ],
+      Languages: [
+        {
+          Name: "İngilizce",
+          Level: "Orta"
+        }
+      ],
+      Socials: [
+          {
+            Name: "Github",
+            Address: "https://github.com/BilalBaydur"
+          },
+          {
+            Name: "Linkedin",
+            Address: "https://www.linkedin.com/in/bilalbaydur/"
+          }
+        ]      
+    }
+
+
+
+
+//İngilizce CV içeriği
+const CvDataEN = {
+  img:"BilalBaydur_img.jpeg",
+  Name: "Bilal",
+  SurName: "Baydur",
+  Gender: "Male",
+  MilitaryService: "Done",
+  DriversLicence: "B",
+  Nationality: "Republic of Türkiye",
+  Email: "xxxxxx@gmail.com",
+  Phone: 5555555555,
+  Address: "İstanbul / Türkiye",
+  CoverLetter: "As someone who has worked in different areas of responsibility throughout "+
+    "my software life, I keep alive the desire and enthusiasm to learn that I had when I first "+
+    "started my career. For this reason, I want to be a part of the big picture by being part of "+
+    "a team that attaches importance to in-company training, can provide a peaceful working environment, "+ 
+    "and most importantly, has adopted this as the company culture.<br/><br/>" +
+    "While researching the impact of software development processes on project success as a master's "+
+    "term project, I had the opportunity to observe it in many projects and teams I was involved in, "+
+    "and once again understood that the positive or negative effects of communication or lack of "+
+    "communication on software development processes affect the project success at least as much as "+
+    "the processes themselves. I found the opportunity. For this reason, I can say that I give "+
+    "communication the value it deserves and try to keep this aspect strong.",
+  Skills:[
+      "Delphi",
+      ".Net Core",
+      "Php",
+      "MsSql",
+      "MySql",
+      "Web Servis, Rest Api",
+      "Entity Framework",
+      "Azure Devops",
+      "Git, Tfs, Svn",
+      "Html, Css, Javascript",
+      "Xml, Xslt",
+      "Postman, Soap UI",
+      "Xamarin"
+  ],
+  Experiences: [
+      {
+        Date: "04.2014 -",
+        Pozision: "SOFTWARE MANAGER",
+        Company: "X YAZILIM A.Ş.",
+        JobDescription: "My job description includes assuming the responsibility of the software "+
+              "development team, ensuring timely and trouble-free delivery of company projects, "+
+              "and auditing the quality of the written code to ensure that the code standards and "+
+              "quality are at the specified standards throughout the software development life cycle, "+
+              "in order to reduce software development costs and facilitate the maintenance process, "+
+              "and of course by writing code for the projects. I can describe it as contributing. <br/><br/>"+
+              "During this process, I actively took part in the development processes of web, desktop "+
+              "and mobile projects for many different sectors such as Erp, CRM, Digital Transformation, "+
+              "Army House and Cinema." ,
+        Skills:  [
+            "Azure Devops",
+            "Git",
+            "Delphi",
+            "C#, .Net Core, Entity Framework, Wpf",
+            "MsSql",
+            "Oracle (For integration needs)",
+            "Xamarin",
+            "Web Servis",
+            "Digital Transformation (e-fatura, e-irsaliye, e-ihracat, E-Defter)",
+            "Html, Css, Javascript"
+          ]
+      },
+      {
+        Date: "05.2009 - 04.2014",
+        Pozision: "SOFTWARE SPECIALIST",
+        Company: "Y YAZILIM A.Ş.",
+        JobDescription: "In general, I was responsible for the development of projects developed with "+
+               "delhpi in line with customer needs. I took part in the development of projects written "+
+               "in C# other than Delphi, or in the development of new projects as web, desktop or mobile "+
+               "applications in line with the needs. I mainly used MsSql as a database.",
+        Skills:[
+            "Delphi",
+            "C#, .Net, Entity Framework",
+            "MsSql",
+            "Oracle (For integration needs)",
+            "TFS (Team Foundation Server)",
+            "Xamarin",
+            "Web Servis",
+            "Digital Transformation (e-fatura)",
+            "Html, Css, Javascript"
+          ]
+      },
+      {
+        Date: "02.2007 - 02.2009",
+        Pozision: "SOFTWARE SPECIALIST",
+        Company: "Z YAZILIM",
+        JobDescription: "Genel olarak delphi ile yazılmış olan projelerin geliştirlmesinde görev aldım. "+
+               "Delphi ile yazılmış olan teknik servis programını php ile web ortamına taşıma sürecinde "+
+               "aktif rol aldım.Yazılımm geliştirme dışında, müşterilere geliştirdiğimiz programlarla ilgili "+
+               "teknik destek verdim.<br/><br/>" +
+               "During this period, I developed web and desktop programs in various projects such as "+
+               "the Dry cleaning program, Technical support program, Personal assistant program.",
+        Skills: [
+            "Delphi",
+            "MsSql, MySql, Access",
+            "Svn (Source Control)",
+            "Php",
+            "Html, Css, Javascript"
+          ]
+      }
+    ],
+  Educations: [
+      {
+        Date: "09.2021 - 02.2023",
+        Departman: "MANAGEMENT INFORMATION SYSTEMS",
+        School: "AHMET YESEVI UNIVERSITY"
+      },
+      {
+        Date: "09.2007 - 06.2010",
+        Departman: "BUSINESS",
+        School: "ANADOLU UNIVERSITY"
+      }
+    ],
+  Languages: [
+    {
+      Name: "Engilish",
+      Level: "Intermediate"
+    }
+  ],
+  Socials: [
+      {
+        Name: "Github",
+        Address: "https://github.com/BilalBaydur"
+      },
+      {
+        Name: "Linkedin",
+        Address: "https://www.linkedin.com/in/bilalbaydur/"
+      }
+    ]      
+}
+
+
+
+    const SettingData = {
+      Lang:"TR", //TR or EN
+      Languages:[
+        {
+          Lang:"TR",
+          PersonelInformationCaption:"KİŞİSEL BİLGİLER",
+          ContactCaption:"İLETİŞİM",
+          SkillsCaption:"YETENEKLER",
+          ExperiencesCaption:"DENEYİMLER",
+          EducationsCaption:"EĞİTİM BİLGİLERİ",
+          ForeignLanguageCaption:"YABANCI DİL",
+          SocialMediaCaption:"SOSYAL MEDYA",
+          GenderCaption:"Cinsiyet",
+          MilitaryCaption:"Askerlik",
+          NationalityCaption:"Uyruk",
+          DriverCaption:"Sürücü Belgesi",
+          EMailCaption:"E-Posta",
+          PhoneCaption:"Telefon",
+          AddressCaption:"Adres",
+          CvData:CvDataTR
+        },  
+        {
+          Lang:"EN",
+          PersonelInformationCaption:"PERSONEL INFORMATİON",
+          ContactCaption:"CONTACT",
+          SkillsCaption:"SKILLS",
+          ExperiencesCaption:"EXPERIENCES",
+          EducationsCaption:"EDUCATIONAL INFORMATION",
+          ForeignLanguageCaption:"FOREIGN LANGUAGE",
+          SocialMediaCaption:"SOCIAL MEDIA",
+          GenderCaption:"Gender",
+          MilitaryCaption:"Military Service",
+          NationalityCaption:"Nationality",
+          DriverCaption:"Driver Licence",
+          EMailCaption:"E-Mail",
+          PhoneCaption:"Phone Number",
+          AddressCaption:"Address",
+          CvData:CvDataEN
+        } 
+      ]
+    }
+
--- script.js
+++ script.js
@@ -1,57 +1,64 @@
+
+
 window.addEventListener("load", (event) => {
-    console.log("page is fully loaded");
+    //Html içindeki başlıklar tespit ediliyor
+    let HtmlCaptionist = document.getElementsByClassName("XmlCaption");  
+     //CV'nin görüntüleme dilini belirliyoruz  
+    let lang = SettingData["Lang"];        
+    let LangData = SettingData["Languages"].find(x=>x.Lang == lang);        
+    console.log(LangData)
 
-    let url = "İnformation.xml";
-    fetch(url)
-    .then(response=>response.text())
-    .then(data=>{
-        //console.log(data);
-        let parser = new DOMParser();
-        //xml okunuyor
-        let xml = parser.parseFromString(data, "application/xml");        
-        
-        //Html içindeki xml ile ilişkilendirilen nesneler alınıyor 
-        let HtmlElementList = document.getElementsByClassName("XmlData");        
+    //Başlıklar istenen dile göre ayarlıyoruz
+    for (let index = 0; index < HtmlCaptionist.length; index++) {
+        const element = HtmlCaptionist[index];
+        CreateElement(element, LangData);
+    }
 
-        //Html dosyasından tespit edilen nesneler içinde dönüyoruz. 
-        for (let i = 0; i < HtmlElementList.length; i++) {            
-            CreateElement(HtmlElementList[i], xml);                          
-        }
+    //Html içindeki Json ile ilişkilendirilen nesneler alınıyor 
+    let HtmlElementList = document.getElementsByClassName("XmlData");        
 
-    });
+    //Html dosyasından tespit edilen nesneler içinde dönüyoruz. 
+    for (let i = 0; i < HtmlElementList.length; i++) {            
+        CreateElement(HtmlElementList[i], LangData.CvData);                          
+    }    
   });
 
+  
 
-  function CreateElement(htmlElement, xml){
 
-    if(htmlElement.className.indexOf("XmlLink") > -1){
-        console.log(htmlElement)
-        let url = xml.getElementsByTagName(htmlElement.id)[0].innerHTML;
+
+
+  function CreateElement(htmlElement, data){
+    
+    if(htmlElement.className.indexOf("XmlLink") > -1){        
+        let url = data[htmlElement.id];
         htmlElement.innerHTML = "<a   href=\"" + url + "\" target=\"_blank\">" + url + "</a>";   
 
     }else  if(htmlElement.className.indexOf("XmlText") > -1){
-        htmlElement.innerHTML = xml.getElementsByTagName(htmlElement.id)[0].innerHTML;            
+        htmlElement.innerHTML = data[htmlElement.id];            
+    }
+    else if(htmlElement.className.indexOf("XmlImg") > -1){
+        htmlElement.src = data[htmlElement.id];            
     }
     else if(htmlElement.className.indexOf("XmlList") > -1){
-        let list = xml.getElementsByTagName(htmlElement.id)[0];
-        
-        for (let j = 0; j < list.childElementCount; j++) {
-            const XmlElement = list.children[j];            
+        let list = data[htmlElement.id];
 
+        for (let j = 0; j < list.length; j++) {                       
             let el = document.createElement("li");
-            el.innerHTML = XmlElement.innerHTML;
+            el.innerHTML = list[j];
             htmlElement.appendChild(el);
         }
-    }else if(htmlElement.className.indexOf("XmlGroup") > -1){
-        console.log(htmlElement.id)
+    }
+    else if(htmlElement.className.indexOf("XmlGroup") > -1){        
         let groupConst = document.getElementById(htmlElement.id);
 
         let groupInnerHtml = groupConst.innerHTML;
         
         groupConst.innerHTML = "";
         
-        let listelement = xml.getElementsByTagName(htmlElement.id)[0];               
-        for (let x = 0; x < listelement.childElementCount; x++) { 
+        let listelement =  data[htmlElement.id];              
+        
+        for (let x = 0; x < listelement.length; x++) { 
         
             let div = document.createElement("div");
             div.innerHTML = groupInnerHtml;
@@ -59,19 +66,19 @@ window.addEventListener("load", (event) => {
             let groupTextElemens = div.getElementsByClassName("XmlText");
             
             for (let y = 0; y < groupTextElemens.length; y++) {                
-                CreateElement(groupTextElemens[y], listelement.children[x]);                     
+                CreateElement(groupTextElemens[y], listelement[x]);                     
             }
 
             let groupLinkElemens = div.getElementsByClassName("XmlLink");
             
             for (let y = 0; y < groupLinkElemens.length; y++) {                
-                CreateElement(groupLinkElemens[y], listelement.children[x]);                     
+                CreateElement(groupLinkElemens[y], listelement[x]);                     
             }            
 
             let groupListElemens = div.getElementsByClassName("XmlList");
             
             for (let y = 0; y < groupListElemens.length; y++) {                
-                CreateElement(groupListElemens[y], listelement.children[x]);                     
+                CreateElement(groupListElemens[y], listelement[x]);                     
             }            
 
             groupConst.appendChild(div)  ;            
